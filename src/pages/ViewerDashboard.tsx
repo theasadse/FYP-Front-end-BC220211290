54bfe7014ee311785729e298a882a0cac2e4d10a
Asad ullah <theasadse@gmail.com>
2025-10-03 21:16:17 +0000

    hello world

--- src/pages/ViewerDashboard.tsx
+++ src/pages/ViewerDashboard.tsx
@@ -4,7 +4,6 @@ import { Card } from 'antd'
 export default function ViewerDashboard() {
   return (
     <div>
-      
       <h2>Viewer Dashboard</h2>
       <Card>Read-only access. View reports and activity summaries here.</Card>
     </div>
