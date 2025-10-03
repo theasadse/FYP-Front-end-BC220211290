abf0deeb68b44b7d53ebc9ee10ae3c63e06e65d7
Asad ullah <theasadse@gmail.com>
2025-10-03 21:00:34 +0000

    h

--- src/pages/UserDashboard.tsx
+++ src/pages/UserDashboard.tsx
@@ -4,6 +4,7 @@ import { Card } from 'antd'
 export default function UserDashboard() {
   return (
     <div>
+      
       <h2>User Dashboard</h2>
       <Card>Welcome, instructor. This dashboard will show your activity logs and stats.</Card>
     </div>
