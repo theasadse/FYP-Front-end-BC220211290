a17ca598b80dddc098433a5043bddc05638576a4
theasadse <theasadse@gmail.com>
2026-02-28 20:28:18 +0500

    fixed issues

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Form,
   Input,
@@ -8,7 +8,6 @@ import {
   Card,
   Typography,
   Layout,
-  Space,
 } from "antd";
 import { useMutation, useQuery } from "@apollo/client";
 import { REGISTER } from "../graphql/operations/auth";
